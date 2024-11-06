69419be5bc831bc2e95092d4c9cabf7f5f1767ca
Gareth Bissland <5170216+gbissland@users.noreply.github.com>
2024-11-06 18:22:08 +1300

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,18 @@
-<php
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ * 
+ * @package FirehawkCRM\SEOPress
+ */
+
+// If uninstall not called from WordPress, exit
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
 
+// Delete plugin options
 delete_option('firehawkcrm_seopress_social_share_image');
 delete_option('firehawkcrm_seopress_title_suffix');
+
+// Clean up any additional plugin data if needed
+// For example, if you add more options in the future
